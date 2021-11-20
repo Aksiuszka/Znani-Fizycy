99a2985bad072b142b2f74ec7d4d22d8416971ce
aksiuszka <aakawecka89@gmail.com>
2021-11-20 19:46:29 +0100

    Struktura pierwszej strony

---
+++ app.js
@@ -0,0 +1,13 @@
+let animationTimeline = gsap.timeline({
+    scrollTrigger:{
+        trigger: ".introSection",
+        start: "0%",
+        end: "100%",
+        markers: false, //markery pokazują gdzie zaczyna się animacja
+        scrub: true, //przy scrollowaniu uaktywnia fromTo
+        pin:true, //przykleja to kółko
+    },
+});
+animationTimeline.fromTo(".introSection", {clipPath: 'circle(10%)'}, {clipPath: 'circle(80%)', duration:4});
+animationTimeline.fromTo(".fizykografika", {opacity:1}, {opacity:0, duration:1},"-=3");
+animationTimeline.fromTo(".introText", {opacity:0}, {opacity:1, duration:1});
